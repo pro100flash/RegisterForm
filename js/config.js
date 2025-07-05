a6fa4325d827d0d2deea0ac5a3360aaedf143fb4
Yuri Khomyn <yuri.khomin@gmail.com>
2025-07-06 02:27:38 +0300

    upd

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 // Конфигурация: URL Web App для Google Sheets
 // Замени YOUR_WEB_APP_URL на реальный URL перед деплоем!
-const GOOGLE_SHEET_URL = "https://script.google.com/macros/s/AKfycbwzRRVhdQ7fO5dCNCi5H4gSdaxxFH5L1O13_0Z613Dp63fkFsj_Obsmgg9fxaon_Jhp2g/exec";
+const GOOGLE_SHEET_URL = "https://script.google.com/macros/s/AKfycbwpYvRv4SkWUFA6Ppw4dNPd5cuVVfHmAFUXT_W5CZqFv_mleTvu2l6Nw5-ECQE3ZPC9DA/exec";
 
 // Проверка конфигурации
 if (!GOOGLE_SHEET_URL || GOOGLE_SHEET_URL.includes("YOUR_WEB_APP_URL")) {
