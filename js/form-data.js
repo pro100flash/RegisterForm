5921adc9882aa0ae865764f9bdb34440768f36e6
Yuri Khomyn <yuri.khomin@gmail.com>
2025-07-06 01:31:40 +0300

    UPD

--- js/form-data.js
+++ js/form-data.js
@@ -1,115 +1,53 @@
-const _0xc9c246 = _0x5063;
-(function (_0x2a974a, _0xc3f7d5) {
-  const _0x1372c9 = _0x5063,
-    _0x34ce64 = _0x2a974a();
-  while (!![]) {
-    try {
-      const _0x482c89 =
-        (parseInt(_0x1372c9(0x13a)) / 0x1) *
-          (parseInt(_0x1372c9(0x147)) / 0x2) +
-        parseInt(_0x1372c9(0x153)) / 0x3 +
-        (-parseInt(_0x1372c9(0x13b)) / 0x4) *
-          (parseInt(_0x1372c9(0x135)) / 0x5) +
-        (-parseInt(_0x1372c9(0x14e)) / 0x6) *
-          (-parseInt(_0x1372c9(0x136)) / 0x7) +
-        -parseInt(_0x1372c9(0x142)) / 0x8 +
-        parseInt(_0x1372c9(0x14c)) / 0x9 +
-        (parseInt(_0x1372c9(0x150)) / 0xa) * (parseInt(_0x1372c9(0x13f)) / 0xb);
-      if (_0x482c89 === _0xc3f7d5) break;
-      else _0x34ce64["push"](_0x34ce64["shift"]());
-    } catch (_0x4d0fe8) {
-      _0x34ce64["push"](_0x34ce64["shift"]());
+// Конфигурация: URL Web App для Google Sheets
+// Замени YOUR_WEB_APP_URL на реальный URL перед деплоем (например, https://script.google.com/macros/s/XYZ789/exec)
+// Не коммить этот файл с реальным URL в публичный репозиторий!
+const GOOGLE_SHEET_URL = "https://script.google.com/macros/s/AKfycbwzRRVhdQ7fO5dCNCi5H4gSdaxxFH5L1O13_0Z613Dp63fkFsj_Obsmgg9fxaon_Jhp2g/exec";
+
+// Проверка конфигурации
+if (!GOOGLE_SHEET_URL || GOOGLE_SHEET_URL.includes("YOUR_WEB_APP_URL")) {
+  console.error("URL не настроен. Замени YOUR_WEB_APP_URL на реальный URL перед деплоем!");
+  alert("Ошибка: URL Google Sheets не настроен!");
+  throw new Error("Конфигурация URL не завершена");
+}
+
+// Функция отправки данных в Google Sheets
+window.sendToGoogleSheet = async (data) => {
+  try {
+    if (!GOOGLE_SHEET_URL || GOOGLE_SHEET_URL.includes("YOUR_WEB_APP_URL")) {
+      return;
     }
-  }
-})(_0x3fd8, 0x6bafa);
-const ENCRYPTED_URL = _0xc9c246(0x139),
-  decodeURL = function (_0x3254b3) {
-    return atob(_0x3254b3);
-  },
-  SHEET_URL = decodeURL(ENCRYPTED_URL);
-function _0x5063(_0x308fad, _0x3c9848) {
-  const _0x3fd848 = _0x3fd8();
-  return (
-    (_0x5063 = function (_0x506335, _0x5df630) {
-      _0x506335 = _0x506335 - 0x134;
-      let _0x3c8de2 = _0x3fd848[_0x506335];
-      return _0x3c8de2;
-    }),
-    _0x5063(_0x308fad, _0x3c9848)
-  );
-}
-(!SHEET_URL || ENCRYPTED_URL === "YOUR_ENCRYPTED_URL") &&
-  (console[_0xc9c246(0x148)](_0xc9c246(0x13e)), alert(_0xc9c246(0x146)));
-window[_0xc9c246(0x138)] = function (_0x1275fe) {
-  const _0x3a3727 = _0xc9c246;
-  if (
-    !SHEET_URL ||
-    ENCRYPTED_URL ===
-      "aHR0cHM6Ly9zY3JpcHQuZ29vZ2xlLmNvbS9tYWNyb3Mvcy9BS2Z5Y2J3elJSVmhkUTdmTzVkQ05DaTVINGdTZGF4eEZINUwxTzEzXzBaNjEzRHA2M2ZrRnNqX09ic21nZzlmeGFvbl9KaHAyZy9leGVj"
-  )
-    return;
-  fetch(SHEET_URL, {
-    method: _0x3a3727(0x141),
-    body: JSON[_0x3a3727(0x14a)](_0x1275fe),
-    headers: { "Content-Type": _0x3a3727(0x14f) },
-  })
-    [_0x3a3727(0x143)]((_0xaf0b57) => _0xaf0b57[_0x3a3727(0x13c)]())
-    [_0x3a3727(0x143)](() => {
-      const _0xea69a5 = _0x3a3727;
-      alert(_0xea69a5(0x145));
-      const _0x4b22f7 = document[_0xea69a5(0x14b)](_0xea69a5(0x149));
-      if (_0x4b22f7) {
-        _0x4b22f7[_0xea69a5(0x137)]();
-        const _0x8752d1 = _0x4b22f7[_0xea69a5(0x14b)](_0xea69a5(0x151));
-        if (_0x8752d1) {
-          const _0x494918 = window[_0xea69a5(0x14d)]["getInstance"](_0x8752d1);
-          _0x494918 && _0x494918[_0xea69a5(0x134)]("");
-          const _0x780339 = _0x4b22f7[_0xea69a5(0x144)](_0xea69a5(0x140));
-          _0x780339["forEach"]((_0xa04192) => _0xa04192[_0xea69a5(0x152)](""));
-        }
-      }
-    })
-    [_0x3a3727(0x13d)]((_0x344a91) => {
-      console["error"](_0x344a91), alert("Ошибка\x20при\x20отправке\x20данных");
+
+    const response = await fetch(GOOGLE_SHEET_URL, {
+      method: "POST",
+      body: JSON.stringify(data),
+      headers: {
+        "Content-Type": "application/json",
+      },
     });
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+
+    await response.text();
+    alert("Данные успешно отправлены в Google Sheets!");
+
+    const form = document.querySelector("form.registration-form");
+    if (form) {
+      form.reset();
+
+      const phoneInput = form.querySelector("input[type='tel']");
+      if (phoneInput) {
+        const iti = window.intlTelInputGlobals.getInstance(phoneInput);
+        if (iti) {
+          iti.setNumber("");
+        }
+        const inputs = form.querySelectorAll("input");
+        inputs.forEach((input) => input.setCustomValidity(""));
+      }
+    }
+  } catch (error) {
+    console.error("Ошибка при отправке данных:", error);
+    alert("Ошибка при отправке данных. Проверь консоль для деталей.");
+  }
 };
-function _0x3fd8() {
-  const _0x435e70 = [
-    "intlTelInputGlobals",
-    "160590RolGtR",
-    "application/json",
-    "10kIywdg",
-    "input[type=\x27tel\x27]",
-    "setCustomValidity",
-    "589227JiSPZz",
-    "setNumber",
-    "19375MbiqRW",
-    "21BrHWbg",
-    "reset",
-    "sendToGoogleSheet",
-    "aHR0cHM6Ly9zY3JpcHQuZ29vZ2xlLmNvbS9tYWNyb3Mvcy9BS2Z5Y2J3elJSVmhkUTdmTzVkQ05DaTVINGdTZGF4eEZINUwxTzEzXzBaNjEzRHA2M2ZrRnNqX09ic21nZzlmeGFvbl9KaHAyZy9leGVj",
-    "4279SCWQYb",
-    "52KQAPey",
-    "text",
-    "catch",
-    "URL\x20не\x20настроен.\x20Закодируй\x20реальный\x20URL\x20и\x20вставь\x20в\x20ENCRYPTED_URL!",
-    "2450283EgNOjs",
-    "input",
-    "POST",
-    "5753128izMBPQ",
-    "then",
-    "querySelectorAll",
-    "Данные\x20успешно\x20отправлены\x20в\x20Google\x20Sheets!",
-    "Ошибка:\x20URL\x20Google\x20Sheets\x20не\x20настроен!",
-    "44RqMSIs",
-    "error",
-    "form.registration-form",
-    "stringify",
-    "querySelector",
-    "5553027bRkylA",
-  ];
-  _0x3fd8 = function () {
-    return _0x435e70;
-  };
-  return _0x3fd8();
-}
